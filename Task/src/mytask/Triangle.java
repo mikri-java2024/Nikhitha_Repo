bf5bcc7032d661e20be2ffcb7341676b42d259bb
Nikhitha <nikhitha@Nikhithas-Air.lan>
2024-05-30 21:34:24 -0400

    Commit

--- Task/src/mytask/Triangle.java
+++ Task/src/mytask/Triangle.java
@@ -4,7 +4,24 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		int i, s, r = 6;
+		
+		for (i=0; i<r; i++)
+			
+			{
+			
+			System.out.print("  ");
+		
+				for (s=0; s<=i; s++)
+				
+					{
+			
+					System.out.println("* ");
+		
+					}	
+		
+			}
 
-	}
-
+}
 }
